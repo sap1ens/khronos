9a3c5767d647da48dc23c449a4801ac9d3a5dfe5
sap1ens <sapiensy@gmail.com>
2015-08-16 13:07:03 -0700

    #5 - Started to implement data module as gen_server

--- src/khronos_data.erl
+++ src/khronos_data.erl
@@ -1,8 +1,80 @@
 -module(khronos_data).
+-behaviour(gen_server).
 
 -define(SERVER, ?MODULE).
 
--export([start_link/0]).
+-include("khronos_data.hrl").
 
-start_link() -> {ok, spawn_link(fun() -> io:format("TDB Data") end)}.
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0, create_check/3, delete_check/1, get_check/1, get_all_checks/0]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+create_check(Id, Type, Interval) ->
+  NewCheck = #check{id = Id, type = Type, interval = Interval},
+  gen_server:call(?SERVER, {add, NewCheck}).
+
+delete_check(Id) ->
+  gen_server:call(?SERVER, {delete, Id}).
+
+get_check(Id) ->
+  gen_server:call(?SERVER, {get, Id}).
+
+get_all_checks() ->
+  gen_server:call(?SERVER, {get_all}).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(_) ->
+  {ok, []}.
+
+handle_call({add, Check}, _From, State) ->
+  {reply, ok, [Check | State]};
+
+handle_call({delete, Id}, _From, State) ->
+  NewState = lists:filter(fun(Check) -> Check#check.id =/= Id end, State),
+  {reply, ok, NewState};
+
+handle_call({get, Id}, _From, State) ->
+  Check = lists:keyfind(Id, #check.id, State),
+  Reply = case Check of
+    false -> {not_found};
+    Result -> {ok, Result}
+  end,
+  {reply, Reply, State};
+
+handle_call({get_all}, _From, State) ->
+  {reply, {ok, State}, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
 
---
+++ test/khronos_data_tests.erl
@@ -0,0 +1,26 @@
+-module(khronos_data_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("khronos_data.hrl").
+
+create_check_test() ->
+  % TODO: extract to a setup method
+  {ok, _} = khronos_data:start_link(),
+
+  khronos_data:create_check(1, tcp, 1000),
+  {ok, Check} = khronos_data:get_check(1),
+
+  ?assertMatch(#check{id = 1, type = tcp, interval = 1000}, Check).
+
+delete_check_test() ->
+  khronos_data:create_check(2, tcp, 1000),
+  khronos_data:create_check(3, udp, 5000),
+  {ok, Checks} = khronos_data:get_all_checks(),
+
+  % TODO: +1 from previous test
+  ?assertEqual(3, length(Checks)),
+
+  khronos_data:delete_check(3),
+
+  {ok, UpdatedChecks} = khronos_data:get_all_checks(),
+  ?assertEqual(2, length(UpdatedChecks)).
