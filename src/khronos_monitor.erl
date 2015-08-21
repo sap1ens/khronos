de47fa6222c00197b2db7a0f0eacda352ee37fc8
sap1ens <sapiensy@gmail.com>
2015-08-21 10:36:03 -0700

    #3 - Init implementation of UDP client

--- src/khronos_monitor.erl
+++ src/khronos_monitor.erl
@@ -1,8 +1,69 @@
 -module(khronos_monitor).
+-behaviour(gen_server).
 
 -define(SERVER, ?MODULE).
 
--export([start_link/0]).
+-include("khronos_data.hrl").
 
-start_link() -> {ok, spawn_link(fun() -> io:format("TDB Monitor") end)}.
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0, stop/0, call_udp/1]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+  gen_server:call(?SERVER, stop).
+
+%% TODO: move to handle_cast
+%% TODO: implement timeout
+call_udp(TargetId) ->
+  {ok, Target} = khronos_data:get_target(TargetId),
+
+  Result = case gen_udp:open(Target#target.port, [{ip, Target#target.address}]) of
+    {ok, _} -> {ok};
+    {error, Msg} -> {failed, Msg} %% Example: {error, eaddrnotavail}
+  end,
+
+  khronos_data:add_metric(TargetId, now(), Result),
+
+  ok.
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(Args) ->
+    {ok, Args}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
 
---
+++ test/khronos_monitor_tests.erl
@@ -0,0 +1,29 @@
+-module(khronos_monitor_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("khronos_data.hrl").
+
+-define(setup(F), {setup, fun start/0, fun stop/1, F}).
+
+khronos_monitor_test_() ->
+  [
+    {"It should call UDP ports targets", ?setup(fun udp_call/1)}
+  ].
+
+start() ->
+  {ok, _} = khronos_data:start_link(),
+  {ok, Name} = khronos_monitor:start_link(),
+  Name.
+
+stop(_) ->
+  khronos_data:stop(),
+  khronos_monitor:stop().
+
+udp_call(_) ->
+  {ok, _} = khronos_data:create_target(1, udp, 8888, {54, 147, 211, 135}, 1000),
+
+  khronos_monitor:call_udp(1),
+
+  {ok, Target} = khronos_data:get_target(1),
+
+  ?_assertMatch([#metric{result = {failed, eaddrnotavail}}], Target#target.metrics).
