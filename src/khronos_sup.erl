a05d90d85b958ed48b9b052036540fb490140b27
sap1ens <sapiensy@gmail.com>
2015-09-07 21:30:38 -0700

    #4 - Add gen_tracker

--- src/khronos_sup.erl
+++ src/khronos_sup.erl
@@ -17,6 +17,14 @@
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+schedule_check(Target) ->
+  %% TODO: replace I with unique name
+  %% TargetWorker = {I, {khronos_monitor, start_link, []}, permanent, 5000, worker, [khronos_monitor]}.
+
+  %% TODO
+  %% {ok, Pid} = gen_tracker:find_or_open(khronos_monitor_sup, TargetWorker).
+  {ok}.
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
@@ -27,13 +35,13 @@ start_link() ->
 %%        |
 %%       sup
 %%     /  |  \
-%%  api  data  monitor_sup
+%%  api  data  monitor_sup (gen_tracker)
 %%             /     \
 %%      monitor_1   monitor_2
 init([]) ->
 %%   Api = ?CHILD(khronos_api, worker),
   Data = ?CHILD(khronos_data, worker),
-  MonitorSup = ?CHILD(khronos_monitor_sup, supervisor),
+  MonitorSup = {khronos_monitor_sup, {gen_tracker, start_link, [khronos_monitor_sup]}, permanent, infinity, supervisor, []},
 
   {ok, { {one_for_one, 5, 60}, [Data, MonitorSup]} }.
 
